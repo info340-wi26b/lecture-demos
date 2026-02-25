fdb3144166b1858134cd612eed2ebfebea6c1c2d
Tim Carlson <timca@arenasports.net>
2026-02-24 19:35:08 -0800

    added sample code for routing

---
+++ react-review/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+export function AboutPage(props) {
+  return (
+    <div className="card bg-gradient">
+      <div className="container card-body">
+        <h2>About React Chat</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati temporibus facere repellat accusamus dicta, sit quasi ipsa, fugit ipsum tempore rerum repellendus ullam doloremque quod porro laborum et recusandae cupiditate?</p>
+      </div>
+    </div>
+  );
+}
--- react-review/src/components/App.jsx
+++ react-review/src/components/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import NavBar from './NavBar';
 import { FECGames } from './FECGames.jsx';
 import { CreateOrEditGame } from './CreateOrEditGame.jsx'
+import { AboutPage } from './About.jsx';
+import { ErrorPage } from './ErrorPage.jsx';
 
 import FECGameList from '../data/FECGameList.json';
 
@@ -12,16 +14,6 @@ function App() {
   const [gameArray, setGameArray] = useState(FECGameList)
 
   const addGame = (gameobj) => {
-
-  // const newGame = {
-  //       "ImagePath": "img/meerkats.jpg",
-  //       "Manufacturer": "TIMS NEW GAME",
-  //       "GameName": "NEW ADDED GAME",
-  //       "ManufacturerSite": "https://www.espn.com"
-  //     }
-
-    // const updateGameArray = [...gameArray, newGame];
-
     const updateGameArray = [...gameArray, gameobj];
     setGameArray(updateGameArray); //update state and re-render
   }
@@ -33,10 +25,50 @@ function App() {
           <NavBar />
           <FECGames FECGameList={gameArray} />
           <CreateOrEditGame addNewGameCallback={addGame}/>
+          <AboutPage />
+          <ErrorPage />
       </div>
     </div>
   );
 }
 
+// // Added react-router and routes to the app component. Also added import statements for Routes and Route at top of file.
+
+// import React, { useState } from 'react';
+// import { Routes, Route } from 'react-router';
+// import NavBar from './NavBar';
+// import { FECGames } from './FECGames.jsx';
+// import { CreateOrEditGame } from './CreateOrEditGame.jsx'
+// import { AboutPage } from './About.jsx';
+// import { ErrorPage } from './ErrorPage.jsx';
+
+// import FECGameList from '../data/FECGameList.json';
+
+// console.log(FECGameList)
+
+// function App() {
+
+//   const [gameArray, setGameArray] = useState(FECGameList)
+
+//   const addGame = (gameobj) => {
+//     const updateGameArray = [...gameArray, gameobj];
+//     setGameArray(updateGameArray); //update state and re-render
+//   }
+  
+
+//   return (
+//     <div className='container'>
+//       <div className="App">
+//          <NavBar />
+//         <Routes>
+//           <Route index element={<FECGames FECGameList={gameArray} />} />
+//           <Route path="/create" element={<CreateOrEditGame addNewGameCallback={addGame}/>} />
+//           <Route path="/about" element={<AboutPage />} />
+//           <Route path="*" element={<ErrorPage />} />
+//         </Routes>
+//       </div>
+//     </div>
+//   );
+// }
 export default App;
 
